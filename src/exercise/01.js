ee8ea15d3f9256096751655c279b5b7e5013d476
sofiaalcm <ledaalcm@gmail.com>
2021-03-23 22:29:06 +0000

    email autofill

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,17 +5,18 @@ import * as React from 'react'
 
 function Greeting() {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState('Fulano') // Transformando em variavel de estado
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name}/>
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
